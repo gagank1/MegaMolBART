49f60bf8991bb764eb33b05f479dbd3be65136e5
Gagan Kaushik <gkaushik@nvidia.com>
2022-08-24 19:51:03 -0700

    using workaround to download public model from ngc api instead of cli

--- setup/config_model.sh
+++ setup/config_model.sh
@@ -15,18 +15,32 @@ fi
 if [ ${PACKAGE} -eq 2 ]
 then
     # install ngc cli
-    wget --content-disposition https://ngc.nvidia.com/downloads/ngccli_linux.zip && unzip ngccli_linux.zip && chmod u+x ngc-cli/ngc
-    find ngc-cli/ -type f -exec md5sum {} + | LC_ALL=C sort | md5sum -c ngc-cli.md5
-    echo "export PATH=\"\$PATH:$(pwd)/ngc-cli\"" >> ~/.bash_profile && source ~/.bash_profile
-
-    # TODO: set ngc token before downloading the model from the ngc registry
+    # wget --content-disposition https://ngc.nvidia.com/downloads/ngccli_linux.zip && unzip ngccli_linux.zip && chmod u+x ngc-cli/ngc
+    # find ngc-cli/ -type f -exec md5sum {} + | LC_ALL=C sort | md5sum -c ngc-cli.md5
+    # echo "export PATH=\"\$PATH:$(pwd)/ngc-cli\"" >> ~/.bash_profile && source ~/.bash_profile
     
+    # mkdir -p /models
+    # cd /models
+    # ngc registry model download-version "nvstaging/clara/megamolbart:0.2.0"
+
+
     # download model from ngc into /models
-    cwd=$(pwd)
-    mkdir -p /models
-    cd /models
-    ngc registry model download-version "nvstaging/clara/megamolbart:0.2.0"
-    cd $cwd
+    # cwd=$(pwd)
+
+    MEGAMOLBART_MODEL_VERSION="0.1"
+    DOWNLOAD_URL="https://api.ngc.nvidia.com/v2/models/nvidia/clara/megamolbart/versions/${MEGAMOLBART_MODEL_VERSION}/zip"
+    MEGAMOLBART_MODEL_PATH="/models"
+
+    mkdir -p ${MEGAMOLBART_MODEL_PATH}
+    wget -q --show-progress \
+            --content-disposition ${DOWNLOAD_URL} \
+            -O ${MEGAMOLBART_MODEL_PATH}/megamolbart_${MEGAMOLBART_MODEL_VERSION}.zip
+    mkdir ${MEGAMOLBART_MODEL_PATH}/megamolbart_v${MEGAMOLBART_MODEL_VERSION}
+    unzip -q ${MEGAMOLBART_MODEL_PATH}/megamolbart_${MEGAMOLBART_MODEL_VERSION}.zip \
+            -d ${MEGAMOLBART_MODEL_PATH}/megamolbart_v${MEGAMOLBART_MODEL_VERSION}
+    
+    
+    # cd $cwd
 
     # download cheminformatics repo for chembench
     mkdir -p /opt/nvidia && \
